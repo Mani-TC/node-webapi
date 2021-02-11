ccfc5d3647174de0eabc05c283fd65b7795ac0b0
TRUNET\manikandch <mani.tc@tru-test.co.nz>
2021-02-12 10:06:11 +1300

    inception

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// reference : https://developer.okta.com/blog/2018/11/15/node-express-typescript
+
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req,res)=>{
+    res.send('hello world');
+});
+
+app.listen(port, ()=>{
+    console.log(`server started listening to port ${port}`)
+});
---
+++ src/readme.md
@@ -0,0 +1,7 @@
+### Reference https://developer.okta.com/blog/2018/11/15/node-express-typescript
+
+## VS Code extensions used
+* ESLint
+* npm intellisense
+* Prettier - Code formatter
+  
